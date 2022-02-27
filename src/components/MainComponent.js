33c149e1bdee9816313ff02586d9b396271892a5
sidharthkumar2k19 <sidharthkumar2k19@gmail.com>
2022-02-27 13:09:47 +0530

    React Redux form revisited

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,10 +9,12 @@ import DishDetail from './DishDetailComponent';
 import About from './AboutComponent';
 import { connect } from 'react-redux';
 import {addComment, fetchDishes} from '../redux/ActionCreators';
+import { actions } from 'react-redux-form';
 
 const mapDispatchToProps = dispatch => ({
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => {dispatch(fetchDishes())}
+  fetchDishes: () => {dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
 });
 
 const mapStateToProps = state => {
@@ -59,7 +61,7 @@ class Main extends Component {
                                         /> } />
           <Route exact path='/menu' element={ <Menu dishes={this.props.dishes}/> } />
           <Route exact path='/menu/:dishId' element={ <DishWithId /> } />
-          <Route path='/contactus' element={ <Contact /> } />
+          <Route path='/contactus' element={ <Contact resetFeedbackForm={this.props.resetFeedbackForm}/> } />
           <Route path='/aboutus' element={ <About leaders={this.props.leaders}/> } />
           <Route path='*' element={ <Navigate to='/home' /> }/>
         </Routes>
